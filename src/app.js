814d3a1494825e3a1e914a49a057d765f7210e0e
Nikita <dragon768@mail.ru>
2021-12-11 18:47:37 +0500

    Task 2, Edit 1

--- src/app.js
+++ src/app.js
@@ -1,26 +1,53 @@
-export const obj = {
-  name: 'мечник',
-  health: 10,
-  level: 2,
-  attack: 80,
-  defence: 40
+// export const obj = {
+//   name: 'мечник',
+//   health: 10,
+//   level: 2,
+//   attack: 80,
+//   defence: 40
+// }
+//
+// export function orderByProps (obj, array) {
+//   let oldObj = {}, newObj = {};
+//
+//   array.forEach(b => Object.keys(obj).forEach(a => {
+//     if (a === b) newObj[b] = obj[a];
+//   }));
+//
+//   for (const prop in obj) {
+//     if (!keyInObj(newObj, prop)) oldObj[prop] = obj[prop];
+//   }
+//   Object.keys(oldObj).sort().forEach(key => newObj[key] = obj[key]);
+//   return newObj;
+// }
+//
+// function keyInObj(obj, key) {
+//   for (const k in obj) if (k === key) return true;
+//   return false;
+// }
+
+export const character = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон'
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...'
+      // <- обратите внимание, описание "засекречено"
+    }
+  ]
 }
 
-export function orderByProps (obj, array) {
-  let oldObj = {}, newObj = {};
-
-  array.forEach(b => Object.keys(obj).forEach(a => {
-    if (a === b) newObj[b] = obj[a];
-  }));
-
-  for (const prop in obj) {
-    if (!keyInObj(newObj, prop)) oldObj[prop] = obj[prop];
-  }
-  Object.keys(oldObj).sort().forEach(key => newObj[key] = obj[key]);
-  return newObj;
-}
-
-function keyInObj(obj, key) {
-  for (const k in obj) if (k === key) return true;
-  return false;
+export function getSkill (num) {
+  return Object.values(character.special).find(skill => skill.id === num);
 }
--- src/app.test.js
+++ src/app.test.js
@@ -1,12 +1,25 @@
-import {orderByProps, obj} from './app.js';
+// import {orderByProps, obj} from './app.js';
+//
+// test('Character Sorting function ', () => {
+//   const objEqual = {
+//     name: 'мечник',
+//     level: 2,
+//     attack: 80,
+//     defence: 40,
+//     health: 10
+//   }
+//   expect(orderByProps(obj, ['level', 'name'])).toEqual(objEqual);
+// });
 
-test('Character Sorting function ', () => {
-  const objEqual = {
-    name: 'мечник',
-    level: 2,
-    attack: 80,
-    defence: 40,
-    health: 10
-  }
-  expect(orderByProps(obj, ['level', 'name'])).toEqual(objEqual);
+import {character, getSkill} from './app.js';
+
+test('Get skill of character (id)', () => {
+  const {id, name, icon, description = 'Описание недоступно'} = getSkill(8);
+  expect(id).toEqual(8);
+});
+
+test('Get skill of character (desc)', () => {
+  const msg = 'Описание недоступно';
+  const {id, name, icon, description = msg} = getSkill(9);
+  expect(description).toEqual(msg);
 });
